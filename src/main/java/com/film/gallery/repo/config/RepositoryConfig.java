00c090138b142f6fc1fe2baa1422ced79ac02bfe
IhorHorchakov <gorchakovigor1993@gmail.com>
2024-10-10 18:35:11 +0200

    Fixed code smells

--- src/main/java/com/film/gallery/repo/config/RepositoryConfig.java
+++ src/main/java/com/film/gallery/repo/config/RepositoryConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(
-//        basePackages = "org.springframework.data.elasticsearch.repositories"
-//        repositoryBaseClass = FilmRepositoryImpl.class
-)
+@EnableElasticsearchRepositories
 class RepositoryConfig {
 }
